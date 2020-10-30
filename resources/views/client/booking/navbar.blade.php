ea6a3d304f3058e920f5bb34c6acdd809cbff65c
vin33t <vineet.chd09@gmail.com>
2020-10-30 12:05:22 +0530

    Booking Nav Update

--- resources/views/client/booking/navbar.blade.php
+++ resources/views/client/booking/navbar.blade.php
@@ -1,35 +1,55 @@
 <nav class="navbar navbar-expand-lg">
-  <button class="btn btn-primary btn-sm">
-  @if(Route::currentRouteName() == 'booking')
-    New Bookings ({{ \App\Client\Booking\Bookings::where('status',NULL)->get()->count() }})
-  @elseif(Route::currentRouteName() == 'booking.processing.mrd')
-    In Processing By MRD  ({{ $bookings->count() }})
-    @elseif(Route::currentRouteName() == 'booking.manager.approved')
-   Approved By Manager ({{ $bookings->count() }})
-    @elseif(Route::currentRouteName() == 'booking.holiday.progress')
-   Holiday In Progress ({{ $bookings->count() }})
-  @endif
 
 
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="{{ route('booking') }}">New </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{ route('booking.processing.mrd') }}">In Processing By MRD</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{ route('booking.manager.approved') }}">Approved By Manager</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{ route('booking.holiday.progress') }}">Holiday In Progress</a>
-      </li>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          @if(Route::currentRouteName() == 'booking')
+            <button class="btn btn-primary btn-sm">
+              <a class="nav-link white" href="{{ route('booking') }}">New Bookings
+                ({{ \App\Client\Booking\Bookings::where('status',NULL)->get()->count() }})</a>
+            </button>
+          @else
+            <a class="nav-link" href="{{ route('booking') }}">New </a>
+          @endif
+        </li>
+        <li class="nav-item">
+          @if(Route::currentRouteName() == 'booking.processing.mrd')
+            <button class="btn btn-primary btn-sm">
+              <a class="nav-link white" href="{{ route('booking.processing.mrd') }}">In Processing By MRD
+                ({{ \App\Client\Booking\Bookings::where('status','approved')->where('offerStatus',NULL)->get()->count() }})</a>
+            </button>
+          @else
+            <a class="nav-link" href="{{ route('booking.processing.mrd') }}">In Processing By MRD
+              ({{ \App\Client\Booking\Bookings::where('status','approved')->where('offerStatus',NULL)->get()->count() }})</a>
+          @endif
+        </li>
+        <li class="nav-item">
+          @if(Route::currentRouteName() == 'booking.manager.approved')
+            <button class="btn btn-primary btn-sm">
+              <a class="nav-link white" href="{{ route('booking.manager.approved') }}">Approved By Manager
+                 ({{ \App\Client\Booking\Bookings::where('status','approved')->where('offerStatus','approved')->doesntHave('ClientHoliday')->get()->count() }})</a>
+            </button>
+          @else
+            <a class="nav-link" href="{{ route('booking.manager.approved') }}">Approved By Manager
+               ({{ \App\Client\Booking\Bookings::where('status','approved')->where('offerStatus','approved')->doesntHave('ClientHoliday')->get()->count() }})</a>
+          @endif
+        </li>
+        <li class="nav-item">
+          @if(Route::currentRouteName() == 'booking.holiday.progress')
+            <button class="btn btn-primary btn-sm">
+              <a class="nav-link white" href="{{ route('booking.holiday.progress') }}">Holiday In Progress   ({{ \App\Client\Booking\Bookings::where('status','approved')->where('offerStatus','approved')->whereHas('ClientHoliday')->get()->count() }})</a>
+            </button>
+          @else
+            <a class="nav-link" href="{{ route('booking.holiday.progress') }}">Holiday In Progress   ({{ \App\Client\Booking\Bookings::where('status','approved')->where('offerStatus','approved')->whereHas('ClientHoliday')->get()->count() }})</a>
+          @endif
+        </li>
 
-    </ul>
-  </div>
+      </ul>
+    </div>
 </nav>
 <hr>
